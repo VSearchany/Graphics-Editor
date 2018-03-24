e89e674ebf03957e44b444b6f2430d4054bc3382
Kolt <vserchenya@mail.ru>
2018-03-24 04:10:16 +0300

    Register model modified

--- Realt/Models/RegisterModel.cs
+++ Realt/Models/RegisterModel.cs
@@ -2,15 +2,8 @@
 
 namespace Realt.Models
 {
-    public class RegisterModel
+    public class RegisterModel : LoginModel
     {
-        [Required]
-        public string Email { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
         [Required]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         [DataType(DataType.Password)]
